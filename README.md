8c41afa6540fca3250715ea4f6280880bcccbb13
Dang Hoang Vu <danghvu@gmail.com>
2012-12-09 13:03:54 +0100

    add LICENSE

--- README.md
+++ README.md
@@ -7,16 +7,20 @@ Small apache module to log body content of request (POST / FORM data). Data are 
 [Sat Dec 08 09:58:43 2012] [info] [client 1.1.1.1] a=100
 ```
 
-Note: You can do the same with mod_security, use this when you want a quick and lightweight solution.
+Note: You can do the same with mod_security, use this when you want a quick and lightweight solution. 
 
-##Installation:
+###Installation:
 ```
 make
 make install
 ```
-##Configuration:
-In `httpd.conf` (optional):
-
+###Configuration:
+In `httpd.conf` (optional):  
 `DumpPostMaxSize 1024`: limit the size of a log entry to `1024` bytes (default value: `1048576` i.e. 1MB)
-
 `DumpPostHeaderAdd Cookie Content-Type`:  add HTTP Header to log together with POST (default value: None)
+
+###Requirement:
+* apxs2 : 
+    * Ubuntu: `sudo apt-get install apache2-threaded-dev`
+    * Others: install apxs, then `alias apxs2=apxs`
+* `LogLevel` of at least `Info`
--- mod_dumpost.h
+++ mod_dumpost.h
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Hoang-Vu Dang <danghvu@gmail.com>
+ * This file is part of mod_dumpost
+ * 
+ * mod_dumpost is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * mod_dumpost is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with mod_dumpost. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 #ifndef __MOD_DUMPOST__
 #define __MOD_DUMPOST__
 
